e43d66fff0608707387c159540b25b56a54f8293
Salomegit <sallygithinji72@gmail.com>
2025-10-29 22:37:15 +0300

    initialised django and next ts

---
+++ frontend/app/home/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+// Define the types for the component's props
+interface MyComponentProps {
+  name: string;
+  age?: number; // Optional prop
+}
+
+const MyComponent: React.FC = () => {
+  return (
+    <div>
+        <h1>hom</h1>
+     
+    </div>
+  );
+};
+
+export default MyComponent
